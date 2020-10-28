271559c05f0403e1fdd40118165f5a20c6a2854c
Markus Mengel <markus.mengelhx@gmail.com>
2020-10-28 17:45:49 +0100

    getting borderRadius values from state

--- src/components/border_square.jsx
+++ src/components/border_square.jsx
@@ -1,9 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 
 import '../style/border_square.css';
 
 const BorderSquare = () => {
- return <div id="border_square"></div>
+  const [borderRadius, setBorderRadius] = useState(
+  {
+    topLeft: "30px",
+    topRight: "10px",
+    bottomLeft: "90px",
+    bottomRight: "40px",
+  });
+ return <div style={{borderTopLeftRadius: borderRadius.topLeft, borderTopRightRadius: borderRadius.topRight, borderBottomLeftRadius: borderRadius.bottomLeft, borderBottomRightRadius: borderRadius.bottomRight}} id="border_square"></div>
 };
 
 
