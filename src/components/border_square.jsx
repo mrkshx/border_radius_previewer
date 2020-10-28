d8e3b4cfe71c464d1d383897c199cae107d63367
Markus Mengel <markus.mengelhx@gmail.com>
2020-10-28 22:26:07 +0100

    added 4 slider to border component

--- src/components/border_square.jsx
+++ src/components/border_square.jsx
@@ -1,4 +1,6 @@
 import React, { useState } from "react";
+import Slider from "rc-slider";
+import 'rc-slider/assets/index.css';
 
 import '../style/border_square.css';
 
@@ -10,7 +12,13 @@ const BorderSquare = () => {
     bottomLeft: "90px",
     bottomRight: "40px",
   });
- return <div style={{borderTopLeftRadius: borderRadius.topLeft, borderTopRightRadius: borderRadius.topRight, borderBottomLeftRadius: borderRadius.bottomLeft, borderBottomRightRadius: borderRadius.bottomRight}} id="border_square"></div>
+
+  return <div style={{borderTopLeftRadius: borderRadius.topLeft, borderTopRightRadius: borderRadius.topRight, borderBottomLeftRadius: borderRadius.bottomLeft, borderBottomRightRadius: borderRadius.bottomRight}} id="border_square">
+    <Slider className="sliderTopLeft" />
+    <Slider className="sliderTopRight" />
+    <Slider className="sliderBottomLeft" />
+    <Slider className="sliderBottomRíght" />
+  </div>
 };
 
 
