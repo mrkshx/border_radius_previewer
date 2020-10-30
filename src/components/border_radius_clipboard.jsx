c1006d5843fb3ad40aba84f0f804d9dcb219569a
Markus Mengel <markus.mengelhx@gmail.com>
2020-10-30 18:12:51 +0100

    fixed cliboard with useEffect

--- src/components/border_radius_clipboard.jsx
+++ src/components/border_radius_clipboard.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import {CopyToClipboard} from 'react-copy-to-clipboard';
 
 import '../style/border_radius_clipboard.css';
@@ -7,8 +7,13 @@ import '../style/border_radius_clipboard.css';
 const BorderRadiusClipboard = (props) => {
   const [clipboardText, copyToClipboard] = useState("");
 
+  useEffect(() => {
+    const value=`border-radius: ${props.value.topLeft}% ${props.value.topRight}% ${props.value.bottomRight}% ${props.value.bottomLeft}%;`
+       copyToClipboard(value);
+   }, [props])
+
   return <div id="border-radius-clipboard" >
-    <input id="border-radius-data" value={`border-radius: ${props.value.topLeft}% ${props.value.topRight}% ${props.value.bottomRight}% ${props.value.bottomLeft}%`} onChange={({target: {value}}) => copyToClipboard(value)} />
+    <input id="border-radius-data" value={clipboardText} onChange={({target: {value}}) => copyToClipboard(value)} />
     <CopyToClipboard text={clipboardText}>
       <button >Copy to clipboard</button>
     </CopyToClipboard>
